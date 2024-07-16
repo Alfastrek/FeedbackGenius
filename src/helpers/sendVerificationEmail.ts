59e730f9ae0bc92593604d6a2fad906203d9eb9a
Aradhya Shukla <aradhya1703@gmail.com>
2024-07-16 05:36:19 +0530

    Sign Up Routing

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,27 @@
 import {resend} from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
+
+import {ApiResponse} from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email:string,
+    username:string,
+    verifyCode:string,
+): Promise<ApiResponse>{
+    try{
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Your FeedbackMatters | Verification Code',
+            react: VerificationEmail({username, otp: verifyCode}),
+        })
+        return {success:true, message:"Verification Email Sent"};
+    } catch (emailError){
+        console.error("Error sending email", emailError);
+        return {
+            success:false,
+            message:"Failed to send Verification Email",
+        };
+
+    }
+}
