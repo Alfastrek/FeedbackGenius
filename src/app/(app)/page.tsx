d3eaf1904caa278b6b1aefe40248e59ab8ac1b7f
Aradhya Shukla <aradhya1703@gmail.com>
2024-08-10 04:32:36 +0530

    UI Backgroud

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <>
       {/* Main content */}
-      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
+      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 text-white home-background">
         <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
             Dive into the World of Anonymous Feedback
@@ -59,9 +59,9 @@ export default function Home() {
       </main>
 
       {/* Footer */}
-      <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2024 FeedBack Genius. All rights reserved.
-      </footer>
+      <footer className="text-center p-2 md:p-3 bg-gray-900 text-white">
+  © 2024 FeedBack Genius. All rights reserved.
+</footer>
     </>
   );
-} 
+}
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -99,7 +99,7 @@ export default function SignUpForm() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+    <div className="flex justify-center items-center min-h-screen bg-gray-800 login-background">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
